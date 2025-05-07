bfb4791426821dff9f625327e395136f5227b0fe
chd428 <chd428@lehigh.edu>
2025-05-07 12:52:26 -0400

    Update project.py - histogram functionality

--- project.py
+++ project.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import scipy.stats as sci
 
 dataset = pd.read_csv('ilidata.csv')
 #dataset tracks influenze like illness (ILI) across US states over time
@@ -15,21 +16,26 @@ states = dataset.state.unique()
 selstate = st.selectbox("Select a state", states)
 
 def numweeks(epiweek):
-    adjustedweek = epiweek - dataset.epiweek[0]
+    adjustedweek = epiweek - startweek
     return adjustedweek
 
 statedata = dataset.loc[dataset["state"] == selstate]
+startweek = statedata['epiweek'].iloc[0]
 statedata = statedata.assign(Week=statedata["epiweek"].astype(int).apply(numweeks))
 #print(statedata)
-
 st.line_chart(statedata, y='ili',x='Week')
 
 
+
 #histogram
+
 fig,ax = plt.subplots()
 mean = np.mean(statedata.ili)
-estim = 1 / mean
-x = np.linspace(0, 10, 500)
-ax.hist(statedata['ili'], bins = 50)
-ax.plot(x, np.exp(estim))
+lambdaval = 1 / mean
+
+count, bins, _ = ax.hist(statedata['ili'], bins = 50, density = True)
+xmin, xmax = 0, np.max(statedata['ili'])
+x = np.linspace(xmin, xmax, 200)
+y = sci.expon.pdf(x, loc = 0, scale = mean)
+ax.plot(x,y)
 st.pyplot(fig)
